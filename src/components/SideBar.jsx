16dc1efe804d056e5db82c25f92532599458d2da
Teshell <fayrongeek@gmail.com>
2022-03-16 19:14:42 +0100

    first commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,103 @@
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
+import styled from "styled-components";
+import SideBarOption from "./SideBarOption";
+
+function SideBar() {
+  return (
+    <SideBarContainer>
+      <SideBarHeader>
+        <SideBarInfo>
+          <h2>PAPA FAM HQ</h2>
+
+          <h3>
+            <FiberManualRecordIcon />
+            Sonny Sangha
+          </h3>
+        </SideBarInfo>
+        <CreateIcon />
+      </SideBarHeader>
+
+      <SideBarOption Icon={InsertCommentIcon} title="Threads" />
+      <SideBarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SideBarOption Icon={DraftsIcon} title="Saved items" />
+      <SideBarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SideBarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SideBarOption Icon={AppsIcon} title="Apps" />
+      <SideBarOption Icon={FileCopyIcon} title="File browser" />
+      <SideBarOption Icon={ExpandLessIcon} title="Show less" />
+
+      <hr />
+      <SideBarOption Icon={ExpandMoreIcon} title="Channels" />
+
+      <hr />
+      <SideBarOption Icon={AddIcon} addChannelOption title="Add Channel" />
+    </SideBarContainer>
+  );
+}
+
+export default SideBar;
+
+const SideBarContainer = styled.div`
+  color: #fff;
+  background-color: var(--slack-color);
+  flex: 0.3;
+  border-top: 1px solid #49274b;
+  max-width: 260px;
+  margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
+`;
+
+const SideBarHeader = styled.div`
+  display: flex;
+  border-bottom: 1px solid #492744;
+  padding: 13px;
+
+  > .MuiSvgIcon-root {
+    padding: 8px;
+    color: #49274b;
+    font-size: 18px;
+    background-color: #fff;
+    border-radius: 999px;
+  }
+`;
+
+const SideBarInfo = styled.div`
+  flex: 1;
+
+  > h2 {
+    font-size: 15px;
+    font-weight: 900;
+    margin-bottom: 5px;
+  }
+
+  > h3 {
+    display: flex;
+    font-size: 13px;
+    font-weight: 400;
+    align-items: center;
+  }
+
+  > h3 > .MuiSvgIcon-root {
+    font-size: 14px;
+    margin-top: 1px;
+    margin-right: 2px;
+    color: green;
+  }
+`;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-v9aZofO0hEwm63swoh4qz-B1P7y8apg",
+  authDomain: "slack-clone-b0b0b.firebaseapp.com",
+  projectId: "slack-clone-b0b0b",
+  storageBucket: "slack-clone-b0b0b.appspot.com",
+  messagingSenderId: "311581345064",
+  appId: "1:311581345064:web:ed0d976aa8b63f08a6fc1d",
+};
